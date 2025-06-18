89407c0d5307e3c68607432f331ccd4a558b6dc0
Duda Kuhn <meduardakuhn@gmail.com>
2025-06-17 21:52:30 -0300

    arrumando erros de vitória

--- main.cpp
+++ main.cpp
@@ -217,7 +217,7 @@ int main(){
                     }
                 }
             }
-            else if(mat[0][0] == mat[0][1] && mat[0][2] == mat[0][0]){
+            else if(mat[0][0] == mat[1][0] && mat[2][0] == mat[0][0]){
                 if(mat[0][0] == 'X'){
                     cout << "Parabéns! Jogador 1 venceu!" << endl;
                     cout << "Deseja reiniciar o jogo? (S/N)" << endl;
@@ -281,7 +281,7 @@ int main(){
                     } 
                 }
             } 
-            else if(mat[2][0] == mat[2][1] && mat[2][1] == mat[2][2]){
+            else if(mat[0][2] == mat[1][2] && mat[1][2] == mat[2][2]){
                 if(mat[2][1] == 'X'){
                     cout << "Parabéns! Jogador 1 venceu!" << endl;
                     cout << "Deseja reiniciar o jogo? (S/N)" << endl;
